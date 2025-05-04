55f0c2d79b3221434306c3987066748f608516ee
km-gupta <kmgupta.official@gmail.com>
2025-05-04 10:36:34 +0530

    Excel Reader Added

--- src/main/java/com/esporcm/base/BaseTest.java
+++ src/main/java/com/esporcm/base/BaseTest.java
@@ -2,35 +2,30 @@ package com.esporcm.base;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
+
+import com.espocrm.config.ConfigReader;
+import com.espocrm.factory.DriverFactory;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BaseTest {
-	
-	public WebDriver driver;
-	
-	//Browser Setup
-	@BeforeSuite
-	public void setup() {
-		WebDriverManager.chromedriver().setup();
-		driver = new ChromeDriver();
-		driver.manage().window().maximize();
-		
-		driver.get("http://localhost:8080/");
-		
-	}
-	
-	
-	//Browser TearDown
-	@AfterSuite
-	public void tearDown() {
-		if(driver != null) {
-			driver.quit();
-		}
-	}
-	
-	
-	
+
+    protected WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+        driver = DriverFactory.initDriver();
+        driver.get(ConfigReader.getAppUrl());
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        DriverFactory.quitDriver();
+    }
 }
--- src/test/java/com/espocrm/tests/LoginSmokeTest.java
+++ src/test/java/com/espocrm/tests/LoginSmokeTest.java
@@ -1,30 +1,59 @@
 package com.espocrm.tests;
 
-import org.openqa.selenium.By;
+import static org.testng.Assert.assertTrue;
+
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
+import com.espocrm.config.ConfigReader;
+import com.espocrm.dataprovider.LoginDataProvider;
+import com.espocrm.pages.DashboardPage;
+import com.espocrm.pages.LoginPage;
 import com.esporcm.base.BaseTest;
 
 public class LoginSmokeTest extends BaseTest {
-	
-	@Test
-	public void verifyPageTitle() throws InterruptedException {
-		//Login
-		Thread.sleep(1000);
-		driver.findElement(By.xpath("//input[@name=\"username\"]")).sendKeys("admin");
-		driver.findElement(By.xpath("//input[@name=\"password\"]")).sendKeys("password");
+
+	@Test(dataProvider = "loginCredentials", dataProviderClass = LoginDataProvider.class)
+	public void testLogin(String username, String password, boolean expectedResult) throws InterruptedException {
+		LoginPage loginPage = new LoginPage(driver);
+		loginPage.login(username, password);
 		
-		driver.findElement(By.xpath("//button[@type=\"submit\"]")).click();
+		DashboardPage dashboardPage = new DashboardPage(driver);
+		boolean actualResult = dashboardPage.isDashboardVisible();
 		
-		
-		Thread.sleep(1000);
-		String title = driver.findElement(By.xpath("//li[@data-name='Home']//span[2]")).getText();
-		System.out.println(title);
-		
-		SoftAssert softAssert = new SoftAssert();
-		softAssert.assertNotEquals(title, "Home", "Login Done");
-		
-//		Assert.assertNotEquals(title, "Home", "Login Done");
+		Assert.assertEquals(actualResult, expectedResult, "Login result does not match expected outcome.");
 	}
+
+	/*
+	 * @Test(description = "Verify that user can login with valid credentials")
+	 * public void testValidLogin() throws InterruptedException { LoginPage
+	 * loginPage = new LoginPage(driver);
+	 * loginPage.login(ConfigReader.getUsername(), ConfigReader.getPassword());
+	 * 
+	 * DashboardPage dashboardPage = new DashboardPage(driver); boolean isLoggedIn =
+	 * dashboardPage.isDashboardVisible();
+	 * 
+	 * Assert.assertTrue(isLoggedIn, "Login failed or dashboard not visible."); }
+	 */
+
+	/*
+	 * @Test public void verifyPageTitle() {
+	 * 
+	 * boolean title = false;
+	 * 
+	 * LoginPage login = new LoginPage(driver); login.login("admin", "password"); //
+	 * Login
+	 * 
+	 * DashboardPage dashboard = new DashboardPage(driver); title =
+	 * dashboard.isDashboardVisible();
+	 * 
+	 * SoftAssert softAssert = new SoftAssert(); softAssert.assertEquals(title,
+	 * true);
+	 * 
+	 * Assert.assertTrue(title, "Login Failed");
+	 * 
+	 * // Assert.assertNotEquals(title, "Dashboard", "Login Done"); }
+	 */
+
 }
